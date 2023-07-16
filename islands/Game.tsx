bdce90c8e25ec7e7a8b0d36f756cfd7ad245a3eb
Sean Sullivan <sullivansean27@gmail.com>
2023-07-16 17:32:15 -0400

    Add description and how-to

--- islands/Game.tsx
+++ islands/Game.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from "preact/hooks";
+import { useEffect, VNode } from "preact/hooks";
 
 interface WasmGameProps {
   name: string
+  description: VNode,
 }
 
-export default function WasmGame({ name }: WasmGameProps) {
+export default function WasmGame({ name, description }: WasmGameProps) {
   useEffect(() => {
     async function startGame() {
       await import(`/wasm/${name.split('-').join('_')}_app_wasm.js`);
     }
     startGame();
   }, []);
-
   return (
     <div class="flex justify-center">
       <canvas id="game-canvas" />
--- routes/cricket-pong.tsx
+++ routes/cricket-pong.tsx
@@ -6,6 +6,32 @@ export default function CricketPong() {
   return (
     <Page repoName="cricket-pong">
       <Game name="cricket-pong" />
+      <div class="my-6 max-w-xl mx-auto">
+        <h2 class="text-lg font-bold">How To Play</h2>
+        <p class="mb-1">
+          Cricket Pong is played by two players, using the QWAS+Space keys for
+          player 1 and UIJK+RShift keys for player 2.
+        </p>
+        <p class="mb-1">
+          Right now there is no score limit or a set number of overs, so play
+          continues until the page is closed.
+        </p>
+        <p class="mb-1 font-semibold">When batting:</p>
+        <ul class="mb-2">
+          <li>Q/U: Swing counter-clockwise</li>
+          <li>W/I: Swing clockwise</li>
+          <li>A/J: Move slowly counter-clockwise</li>
+          <li>S/K: Move slowly clockwise</li>
+        </ul>
+        <p class="mb-1 font-semibold">When fielding:</p>
+        <ul>
+          <li>Q/U: Rotate outfield counter-clockwise</li>
+          <li>W/I: Rotate outfield clockwise</li>
+          <li>A/J: Rotate infield counter-clockwise</li>
+          <li>S/K: Rotate infield clockwise</li>
+          <li>Space/RShift: Bowl the ball</li>
+        </ul>
+      </div>
     </Page>
   );
 }
